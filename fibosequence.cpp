b90ccd8ab7b83db68baf7be0a7b1e79ac30ffe8a
bhavyasrijan <78565310+bhavyasrijan@users.noreply.github.com>
2021-04-08 14:41:50 +0530

    Add files via upload

---
+++ fibosequence.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+int n1=0,n2=1,n3,i,num;
+
+cout<<"upto which pos you want fibo sequence"<<endl;
+
+cin>>num;
+cout<<n1<<" , "<<n2;
+
+for ( i = 2; i <num; i++)
+{
+    n3=n1+n2;
+    cout<<" , "<<n3;
+
+     n1=n2;
+
+     n2=n3;
+     
+}
+
+
+
+
+
+
+
+
+return 0;
+}
