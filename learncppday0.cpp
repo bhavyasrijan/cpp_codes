8388f2b5b3d55a1dddbf351ccfd1153d549120b5
bhavyasrijan <78565310+bhavyasrijan@users.noreply.github.com>
2021-04-09 14:25:06 +0530

    Add files via upload

---
+++ learncppday0.cpp
@@ -0,0 +1,127 @@
+//Q1
+
+/*#include<iostream>
+using namespace std;
+
+int main(){
+  int a,b;
+
+  cout<<"enter two numbers"<<endl;
+  cin>>a>>b;
+  if (a>b)
+  {
+      cout<<"the greater number is "<<a<<endl;
+  }
+  else
+  cout<<"the greater number is "<<b<<endl;
+return 0;
+}*/
+
+
+//Q2
+
+/*#include<iostream>
+using namespace std;
+
+int main(){
+int a,b,temp;
+
+  cout<<"enter two numbers"<<endl;
+  cin>>a>>b;
+  cout<<"a= "<<a<<endl<<"b= "<<b<<endl;
+  temp=a;
+  a=b;
+  b=temp;
+  cout<<"after swapping : a= "<<a<<" and b= "<<b<<endl;
+return 0;
+}*/
+
+
+
+//Q3
+
+/*#include<iostream>
+using namespace std;
+
+int main(){
+  int a,b,c;
+
+  cout<<"enter three numbers"<<endl;
+  cin>>a>>b>>c;
+  cout<<"a= "<<a<<endl<<"b= "<<b<<endl<<"c= "<<c<<endl;
+  string (result)= (a>b)? (a>c)? "a is greatest":"c is greatest" : (b>c)? "b is greatest":"c is greatest";
+  cout<<result<<endl;
+return 0;
+}*/
+
+
+
+//Q4
+
+
+/*#include<iostream>
+using namespace std;
+
+int main(){
+
+       int a;
+       cout<<"enter a year to check if it is a leap year "<<endl;
+       cin>>a;
+
+       if(a%4==0 && a%100!=0)
+       cout<<"it is a leap year"<<endl;
+        
+       else if (a%400==0)
+       cout<<"it is a leap year"<<endl;
+      
+      else
+      cout<<"it is not a leap year"<<endl;
+
+
+
+return 0;
+}*/
+
+
+
+//Q5
+
+
+/*#include<iostream>
+using namespace std;
+
+int main(){
+   
+   int n1=0,n2=1,n3,i,num;
+
+   cout<<"which place number do you want of fibo sequence"<<endl;
+   cin>>num;
+
+   for ( i = 1; i < num; i++)
+   {
+     n3=n1+n2;
+     n1=n2;
+     n2=n3;
+   }
+    
+     cout<<"the "<<num<<"th fibonacci number is "<<n3<<endl;
+  
+
+return 0;
+}*/
+
+
+
+//Q6
+
+
+  #include<iostream>
+  using namespace std;
+  
+  #include<iostream>
+  using namespace std;
+  
+  int main(){
+  int
+  return 0;
+  }
